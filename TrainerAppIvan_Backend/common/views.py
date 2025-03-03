6e2878e2177674d970df1af7e1ae7ba78459883a
VladimirSiTozi <132054352+VladimirSiTozi@users.noreply.github.com>
2025-03-03 23:29:53 +0200

    Create CoachingPageView and fix his template

--- TrainerAppIvan_Backend/common/views.py
+++ TrainerAppIvan_Backend/common/views.py
@@ -4,3 +4,9 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'common/home.html'
+
+
+class CoachingPageView(TemplateView):
+    template_name = 'common/coaching.html'
+
+
